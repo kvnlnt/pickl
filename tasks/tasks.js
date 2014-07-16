8bf0e295acf22b37a65ea2a916570f3dd0cc7893
kvnlnt <kvnlnt@yahoo.com>
2014-07-15 17:43:44 -0700

    build cleanup ;

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -3,5 +3,9 @@ module.exports = function(grunt) {
     grunt.registerTask('build', [
         'concat', 'uglify', 'cssmin'
     ]);
+
+    grunt.registerTask('install', [
+    	'curl'
+    ]);
     
 };
