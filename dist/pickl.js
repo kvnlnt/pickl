bc588b0e256ce6b46dea57cf8e7232ff80117571
kvnlnt <kvnlnt@yahoo.com>
2014-07-30 18:28:18 -0700

    build it

--- dist/pickl.js
+++ dist/pickl.js
@@ -20,30 +20,109 @@ var Pickl = function(options){
 
 };;Pickl.prototype.Config = (function(){
 
-	var f1        = {};
-	f1.name       = 'Orientation';
-	f1.values     = ['righty','lefty'];
-	f1.selected   = 0;
+	var config = {
 
-	var f2        = {};
-	f2.name       = 'Instrument';
-	f2.values     = ['guitar','banjo','ukelele'];
-	f2.selected   = 0;
+		title:'Options',
+		fields:{
+			orientation:{
+				name:'orientation',
+				value:'righty',
+				enabled:true,
+				callback:null,
+				options:{
+					righty:{
+						name:'righty',
+						value:'RIGHTY'
+					},
+					lefty:{
+						name:'lefty',
+						value:'LEFTY'
+					}
+				}
+			},
+			longPick:{
+				name:'long pick',
+				value:'1',
+				enabled:true,
+				options:{
+					1:{ name:'1', value:'1'},
+					2:{ name:'2', value:'2 '},
+					3:{ name:'3', value:'3 '},
+					4:{ name:'4', value:'4 '},
+					5:{ name:'5', value:'5 '},
+					6:{ name:'6', value:'6 '},
+					7:{ name:'7', value:'7 '},
+					8:{ name:'8', value:'8 '},
+					9:{ name:'9', value:'9 '},
+					10:{ name:'10', value:'10 '},
+					11:{ name:'11', value:'11 '},
+					12:{ name:'12', value:'12 '},
+					13:{ name:'13', value:'13 '},
+					14:{ name:'14', value:'14 '},
+					15:{ name:'15', value:'15 '},
+					16:{ name:'16', value:'16 '},
+				}
+			},
+			instrument:{
+				name:'instrument',
+				value:'guitar',
+				enabled:true,
+				callback:null,
+				options:{
+					guitar:{
+						name:'guitar',
+						value:'GUITAR',
+						enable:'tuning_guitar',
+						disable:'tuning_banjo'
+					},
+					banjo:{
+						name:'banjo',
+						value:'BANJO',
+						disable:'tuning_guitar',
+						enable:'tuning_banjo'
+					}
+				}
+			},
+			tuning_guitar:{
+				name:'tuning',
+				value:'standard',
+				enabled:true,
+				options:{
+					standard:{
+						name:'standard',
+						value:'EADGBE'
+					},
+					drop_d:{
+						name:'drop d',
+						value:'DADGBE'
+					}
+				}
+			},
+			tuning_banjo:{
+				name:'tuning',
+				value:'standard',
+				enabled:false,
+				options:{
+					standard:{
+						name:'standard',
+						value:'EADG'
+					},
+					drop_d:{
+						name:'drop d',
+						value:'DADG'
+					}
+				}
+			}
+		}
 
-	var f3        = {};
-	f3.name       = 'Strings';
-	f3.values     = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
-	f3.selected   = 0;
-
-	var config    = {};
-	config.title  = 'Options';
-	config.fields = [f1, f2, f3];
+	};
 
 	return config;
 
 }());;Pickl.prototype.render = function(){
 
-	this.pickl            = this.renderForm();
+	this.pickl 		      = this.pickl || this.renderForm();
+	this.pickl.clear();
 	this.pickl.background = this.renderBackground();
 	this.pickl.title      = this.renderTitle();
 	this.pickl.close      = this.renderClose();
@@ -102,15 +181,20 @@ Pickl.prototype.renderFields = function(){
 	var fields = this.pickl.g().attr('class','fields');
 	var y      = 90;
 
-	_.each(this.config.fields, function(field, i){
+	_.each(this.config.fields, function(field){
 
-		var fieldGroup  = fields.g().attr({ 'class':'field button', 'transform':'translate('+layout.x+','+y+')' });
-		var fieldTarget = fieldGroup.rect(0,0,layout.width, 40).attr({ 'class':'touchTarget' });
-		var fieldTitle  = fieldGroup.text(layout.x + 70,20, field.name).attr({ 'class':'title' });
-		var fieldValue  = fieldGroup.text(layout.x + 80, 20, field.values[field.selected]).attr('class','value');		
-		y 			   += 41;
+		if(field.enabled){
 
-		fieldGroup.click(function(){that.displayOptions(i)},that);
+			var fieldGroup  = fields.g().attr({ 'class':'field button', 'transform':'translate('+layout.x+','+y+')' });
+			var fieldTarget = fieldGroup.rect(0,0,layout.width, 40).attr({ 'class':'touchTarget' });
+			var fieldTitle  = fieldGroup.text(layout.x + 70,20, field.name).attr({ 'class':'title' });
+			var fieldValue  = fieldGroup.text(layout.x + 80, 20, field.options[field.value].name).attr('class','value');		
+			y 			   += 41;
+
+			var click = void 0 === field.callback || null === field.callback ? function(){that.displayOptions(field)} : field.callback;
+			fieldGroup.click(click,that);
+
+		}
 
 	});
 
@@ -118,44 +202,109 @@ Pickl.prototype.renderFields = function(){
 
 };
 
-Pickl.prototype.renderOptions = function(fieldIndex){
+Pickl.prototype.renderOptions = function(field){
 
-	var that        = this;
-	var layout      = that.calcLayout().fields;
-	var options     = this.pickl.g().attr({ 'class':'options' });
-	var config      = this.config.fields[fieldIndex];
-	var background  = options.rect(0,0,that.form.width, that.form.height).attr('class','background');
-	var title       = options.text(that.form.width/2, 27, config.name).attr('class','title options');
-	var fields      = options.g().attr({ 'class':'fields' });
-	var selected    = config.values[config.selected];
-	var y           = 50;
-	var x           = layout.x - 1;
-	var field       = this.pickl.fields[fieldIndex];
+	var that          = this;
+	var layout        = that.calcLayout().fields;
+	var options       = this.pickl.g().attr({ 'class':'options' });
+	var background    = options.rect(0,0,that.form.width, that.form.height).attr('class','background');
+	var title         = options.text(that.form.width/2, 27, field.name).attr('class','title options');
+	var fieldsHeight  = that.form.height-140;
+	var isClipping    = (_.size(field.options) * 40) > fieldsHeight;
+	var fieldsShowing = Math.floor(fieldsHeight/40);
+	var fieldHeight   = isClipping ? fieldsHeight/fieldsShowing : 40;
+	var fieldMaskRect = options.rect(layout.x,50,layout.width, fieldsHeight + (fieldsShowing)).attr({'fill':'#FFFFFF'});
+	var fieldMask     = options.mask().add(fieldMaskRect).attr({'class':'mask'});
+	var fieldWrapper  = options.g().attr({ 'class':'fields', mask:fieldMask });
+	var fields        = fieldWrapper.g().data('y',0);
+	var selected      = field.options[field.value].value;
+	var selectedIndex = 0;
+	var y             = 50;
+	var x             = layout.x - 1;
+	var scrollBtnY    = this.form.height - 70;
+	var scrollClass   = true === isClipping ? '' : 'hide';
+	var down          = options.g().attr({ 'class':'field scroll ' + scrollClass, 'transform':'translate('+this.form.width/2+','+scrollBtnY+')' });
+	var scrolling     = true;
+	var downTarget    = down.rect(0,0,layout.width/2, fieldHeight).attr({ 'class':'touchTarget' });
+	var downText      = down.text(layout.width/4,fieldHeight/2,'*').attr({ 'class':'value' });
+	var up            = options.g().attr({ 'class':'field scroll ' + scrollClass, 'transform':'translate('+layout.x+','+scrollBtnY+')' });
+	var upTarget      = up.rect(0,0,layout.width/2, fieldHeight).attr({ 'class':'touchTarget' });
+	var upText        = up.text(layout.width/4,fieldHeight/2,'*').attr({ 'class':'value' });
 
+	// update icons
+	upText.node.innerHTML 	= '&#xf077';
+	downText.node.innerHTML = '&#xf078';
+
+	// Scrolling logic
+	var scrollDownStart = function(){ scrolling = true; scrollDown(); };
+	var scrollStop 		= function(){ scrolling = false; };
+	var scrollDown 		= function(){
+		var y = fields.data('y') - (fieldHeight + 1);
+		var threshold = -(_.size(field.options) - fieldsShowing) * (fieldHeight + 1); // num of fields minus num of fields possible to show time field height
+		if(Math.ceil(y) >= threshold){
+			fields.animate({'transform':'translate(0,'+y+')'}, 100, mina.easeout, function(){
+				if(scrolling) scrollDown();
+			});
+			fields.data('y',y);
+		}
+
+	};
+
+	var scrollUpStart = function(){ scrolling = true; scrollUp(); };
+	var scrollUp 	  = function(){
+		var y = fields.data('y') + (fieldHeight + 1);
+		if(Math.floor(y) <= 0){
+			fields.animate({'transform':'translate(0,'+y+')'}, 100, mina.easeout, function(){
+				if(scrolling) scrollUp();
+			});
+			fields.data('y',y);
+		}
+	};
+
+	down.mousedown(scrollDownStart);
+	down.mouseup(scrollStop);
+	up.mousedown(scrollUpStart);
+	up.mouseup(scrollStop);
+
+	// position options container
 	options.attr({ 'transform':'translate('+this.form.width+',0)' });
 
 	// option fields
-	_.each(config.values, function(value, i){
+	_.each(field.options, function(option, k){
 
 		var fieldGroup  = fields.g().attr({ 'class':'field', 'transform':'translate('+x+','+y+')' });
-		var fieldTarget = fieldGroup.rect(0,0,layout.width/2, 40).attr({ 'class':'touchTarget' });
-		var fieldText   = fieldGroup.text(40,20, value).attr({ 'class':'value' });
-		var klass       = value === selected ? 'check selected' : 'check';
+		var fieldTarget = fieldGroup.rect(0,0,layout.width, fieldHeight).attr({ 'class':'touchTarget' });
+		var fieldText   = fieldGroup.text(fieldHeight,fieldHeight/2, option.name).attr({ 'class':'value' });
+		var klass       = option.value === selected ? 'check selected' : 'check';
 		var check       = fieldGroup.g().attr({ 'class':klass});
-		var checkTarget = check.rect(0,0,40,40).attr({ 'class':'touchTarget' });
-		var checkMark   = check.text(10,20,'*').attr({ 'class':'value' });
+		var checkTarget = check.rect(0,0,fieldHeight,fieldHeight).attr({ 'class':'touchTarget' });
+		var checkMark   = check.text(fieldHeight/4,fieldHeight/2,'*').attr({ 'class':'value' });
+
+		if(option.value === selected){
+			selectedIndex = k;
+		}
 
 		checkMark.node.innerHTML = '&#xf00c';
-		y = i % 2 === 0 ? y : y + 41;
-		x = i % 2 === 0 ? layout.width/2 + layout.x + 1 : layout.x - 1;
+		y += fieldHeight + 1;
 
 		check.click(function(){ 
-			config.selected = i;
+			field.value = k;
 			options.animate({ 'transform':'translate('+this.form.width+',0)'}, 200, mina.easeout, function(){ this.remove(); });
-			that.pickl.fields[fieldIndex].select('.value').node.textContent = value;
+			that.displayField(option.disable, false);
+			that.displayField(option.enable, true);
+			that.render();
 		}, that);
 
 	});
+
+	// auto scroll to selected item if it's hidden
+	if(selectedIndex > fieldsShowing){
+		var y = -(selectedIndex - fieldsShowing) * (fieldHeight + 1);
+		fields.attr({'transform':'translate(0,'+y+')'});
+		fields.data('y',y);
+
+	}
+	console.log(selectedIndex, fieldsShowing);
 
 	return options;
 
@@ -187,18 +336,30 @@ Pickl.prototype.calcFields = function(){
 
 };
 
-Pickl.prototype.displayOptions = function(fieldIndex){
+Pickl.prototype.displayOptions = function(field){
 
-	// console.log(field);
-	var options = this.renderOptions(fieldIndex);
+	var options = this.renderOptions(field);
 	options.animate({'transform':'translate(0,0)'}, 200, mina.easin);
+
+};
+
+Pickl.prototype.displayField = function(fields, enabled){
+
+	var that = this;
+	var fields = "string" === typeof fields ? [fields] : fields;
+
+	_.each(fields,function(field){
+
+		if(void 0 !== that.config.fields[field]) that.config.fields[field].enabled = enabled;
+
+	});
 
 };;Pickl.prototype.save = function(){
 
 	var picks = {};
 
 	_.each(this.config.fields, function(field){
-		picks[field.name] = field.values[field.selected];
+		if(field.enabled) picks[field.name] = field.options[field.value].value;
 	});
 	
 	this.callback(picks);
@@ -224,7 +385,7 @@ Pickl.prototype.displayOptions = function(fieldIndex){
 	themes.zen.titleColor            = '#FFFFFF';
 	themes.zen.buttonTextColor       = '#999999';
 	themes.zen.buttonTextColorOver   = '#FFFFFF';
-	themes.zen.buttonStroke          = '#333333';
+	themes.zen.buttonStroke          = '';
 	themes.zen.modalBackground       = '#000000';
 
 	return themes;
@@ -263,7 +424,8 @@ Pickl.prototype.themeLoad = function(){
 		css += '.pickl .field .title { font-size: .7rem; opacity: 0.5; text-anchor: end; }';
 		css += '.pickl .field .value { text-anchor:start; }';
 		css += '.pickl .field .check { font-family:\'FontAwesome\'; font-size: 1.25rem; text-anchor:middle; opacity: .2; cursor: pointer; }';
-		css += '.pickl .field .check.selected, .pickl .field .check:hover { opacity: 1; }';
+		css += '.pickl .scroll { font-family:\'FontAwesome\'; font-size: 1.25rem; text-anchor:middle; opacity: .8; cursor: pointer; }';
+		css += '.pickl .field .check.selected, .pickl .field .check:hover, .pickl .scroll:hover { opacity: 1; }';
 
 		// overrides
 		css += '.pickl .background { fill: '+style.background+'; } ';
--- tasks/options/curl.js
+++ tasks/options/curl.js
@@ -1,6 +1,5 @@
 module.exports = {
 	'libs/jquery.js'   : 'http://code.jquery.com/jquery-2.1.1.min.js',
 	'libs/lodash.js'   : 'https://raw.githubusercontent.com/kvnlnt/lodash/master/dist/lodash.compat.min.js',
-	'libs/snap.js' 	   : 'https://raw.githubusercontent.com/kvnlnt/Snap.svg/master/dist/snap.svg-min.js',
-	'libs/hammer.js'   : 'https://raw.githubusercontent.com/kvnlnt/hammer.js/master/hammer.min.js',
+	'libs/snap.js' 	   : 'https://raw.githubusercontent.com/kvnlnt/Snap.svg/master/dist/snap.svg-min.js'
 }
